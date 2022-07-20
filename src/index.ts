1ce30b98bc0f2bcb564732bd16e8de1dafe7c9b5
Caleb Gasser <1561693+calebgasser@users.noreply.github.com>
2022-07-20 15:58:32 -0500

    chore: workflow changes again

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+/**
+ * Coment
+ */
 export { default as default } from './GhBlog.svelte';
 export { default as GhBlog } from './GhBlog.svelte';
 export { default as GhBlogIndex } from './GhBlogIndex.svelte';
