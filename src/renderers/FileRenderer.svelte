4144f4dd698dd4357b8ab355e04ee3ef5d8cc428
Caleb Gasser <1561693+calebgasser@users.noreply.github.com>
2022-07-25 18:20:39 -0500

    chore: updated default render

--- src/renderers/FileRenderer.svelte
+++ src/renderers/FileRenderer.svelte
@@ -1,13 +1,18 @@
 <script type="text/typescript">
 	export let name = '';
 	export let setAsDisplayPage: () => any;
+	export let isSelected = false;
 </script>
 
 <li>
-	<span on:click={setAsDisplayPage}>{name}</span>
+	<span class:isSelected on:click={setAsDisplayPage}>{name}</span>
 </li>
 
 <style>
+	.isSelected {
+		color: aquamarine;
+	}
+
 	span {
 		padding: 0 0 0 1.5em;
 		background: 0 0.1em no-repeat;
